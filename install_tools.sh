0f99b968192727be9b0ea761fc4ad58d9d2002ed
Fr3dd1e <freddiehacks@protonmail.com>
2021-10-03 21:59:27 +0100

    updated osint

--- install_tools.sh
+++ install_tools.sh
@@ -8,26 +8,21 @@ apt install tmux -y
 apt install git -y
 apt install vim nano -y
 apt install okular eom ghex -y
+
 apt install python3 python -y
 apt install python3-pip python-pip -y
+python3 -m pip install pyinstaller
+
 apt install golang-go -y
+apt install ruby -y
+
 apt install ncat -y 
 
-## PIP
-pip3 install pyinstaller
 sudo chmod 777 /opt
 
-# FORENSICS / OSINT
+# FORENSICS
 apt install strings binwalk steghide -y
-cd /opt
-git clone https://github.com/sherlock-project/sherlock
-cd sherlock
-python3 -m pip install -r requirements.txt
-
-cd /opt
-git clone https://github.com/Datalux/Osintgram.git
-cd Osintgram
-python3 -m pip install -r requirements.txt
+gem install zsteg
 
 ## Stegseek
 wget https://github.com/RickdeJager/stegseek/releases/download/v0.6/stegseek_0.6-1.deb -O /opt
@@ -37,6 +32,15 @@ rm /opt/stegseek_0.6-1.deb
 pip install stringcheese
 # todo: stringcheese, stegseek, ghidra
 
+# OSINT
+git clone https://github.com/sherlock-project/sherlock /opt/sherlock
+cd /opt/sherlock
+python3 -m pip install -r requirements.txt
+
+git clone https://github.com/Datalux/Osintgram.git /opt/Osintgram
+cd /opt/Osintgram
+python3 -m pip install -r requirements.txt
+
 # PENTEST
 ## FFUF
 git clone https://github.com/ffuf/ffuf /opt/ffuf
@@ -44,5 +48,8 @@ cd /opt/ffuf
 go get
 go build
 ln -s /opt/ffuf/ffuf /usr/bin/ffuf
+
+## Burpsuite
+apt install openjdk-8-jre -y
 
 # john, hashcat, seclists, smbclient, smbmap, enum4linux, impacket, sqlmap, ftp, showmount, redis-cli, sshpass
