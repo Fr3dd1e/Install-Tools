21e27ca0d366ef9a893c63fe6e3a9412c418bbb7
Fr3dd1e <freddiehacks@protonmail.com>
2021-10-03 16:29:15 +0100

    another update

--- install_tools.sh
+++ install_tools.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-sudo apt upgrade -y
-sudo apt update -y
-
-
-# TMUX
-sudo apt install tmux -y
-echo "# TMUX Config file" > ~/.tmux.conf
-echo "set-option -g history-limit 10000" >> ~/.tmux.conf
-echo "set -g prefix C-space" >> ~/.tmux.conf
-echo "bind | split-window -v" >> ~/.tmux.conf
-echo "bind - split-window -h" >> ~/.tmux.conf
+apt upgrade -y
+apt update -y
 
 # MISC
-sudo apt install git -y
-sudo apt install vim nano -y
-sudo apt install okular eom ghex -y
+apt install tmux -y
+apt install git -y
+apt install vim nano -y
+apt install okular eom ghex -y
+apt install python3 python -y
+apt install python3-pip python-pip -y
+apt install golang-go -y
+apt install ncat -y 
+
+## PIP
+pip3 install pyinstaller
 sudo chmod 777 /opt
 
 # FORENSICS
-sudo apt install strings binwalk steghide -y
+apt install strings binwalk steghide -y
+
+## Stegseek
+wget https://github.com/RickdeJager/stegseek/releases/download/v0.6/stegseek_0.6-1.deb -O /opt
+dpkg -i /opt/stegseek_0.6-1.deb
+rm /opt/stegseek_0.6-1.deb
+
+pip install stringcheese
 # todo: stringcheese, stegseek, ghidra
+
+# PENTEST
+## FFUF
+git clone https://github.com/ffuf/ffuf /opt/ffuf
+cd /opt/ffuf
+go get
+go build
+ln -s /opt/ffuf/ffuf /usr/bin/ffuf
+
+# john, hashcat, seclists, smbclient, smbmap, enum4linux, impacket, sqlmap, ftp, showmount, redis-cli, sshpass
